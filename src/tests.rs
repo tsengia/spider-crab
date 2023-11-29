f773a0b1fc9c444f60bba0ac74ffa2083e47c594
Tyler Sengia <tylersengia@gmail.com>
2023-11-29 18:56:00 -0500

    Fully implementing test_simple_page()

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,9 @@
 use mockito::Server;
 
-use crate::{SpiderOptions, PageGraph, PageMap, Page};
+use crate::SpiderCrab;
 
-#[test]
-fn test_simple_page() {
+#[tokio::test]
+async fn test_simple_page() {
     let mut server = Server::new();
 
     let url = server.url();
@@ -14,8 +14,10 @@ fn test_simple_page() {
       .with_body("<!DOCTYPE html><html><body><a href=\"https://example.com\" >Example Link</a></body></html>")
       .create();
 
-    let mut options = SpiderOptions::new(&url);
-    
-    let mut page_graph = Mutex::<PageGraph>::(PageGraph::new());
+    let spider_crab = SpiderCrab::new(url.as_str());
 
+    let success = spider_crab.visit_website(url.as_str()).await;
+    assert!(success);
+
+    mock.assert();
 }
