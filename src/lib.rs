9ee0e11cd61fe5c5f27feeacf3e0b85b3f27582d
Tyler Sengia <tylersengia@gmail.com>
2023-12-10 22:11:14 -0500

    Add dot module

--- src/lib.rs
+++ src/lib.rs
@@ -8,16 +8,19 @@ use url::{Host, Url};
 
 pub mod algo;
 pub mod error;
+pub mod dot;
 pub mod url_helpers;
 
 #[cfg(test)]
 pub mod tests;
 
+#[derive(Debug)]
 pub struct Link {
     pub html: String,
 }
 
 /// Representation of a document/page
+#[derive(Debug)]
 pub struct Page {
     /// Title of the page
     pub title: Option<String>,
