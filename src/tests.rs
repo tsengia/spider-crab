71108f2c7232ac39754005d186f7c59cdbf400e0
Tyler Sengia <tylersengia@gmail.com>
2023-12-13 22:48:46 -0500

    Use helper functions in tests and add test for content type

--- src/tests.rs
+++ src/tests.rs
@@ -26,20 +26,14 @@ async fn test_simple_page() {
     // Make sure the HTTP request was made
     mock.assert();
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains two pages
-    assert_eq!(graph.node_count(), 2);
+    assert_eq!(spider_crab.page_count(), 2);
 
     // Make sure there is only one link in the page graph
-    assert_eq!(graph.edge_count(), 1);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 1);
 
     // Make sure that the page map contains the mock page
-    assert!(map.contains_key(&parsed_url));
-
-    // Make sure there are two pages in the page map
-    assert_eq!(map.len(), 2);
+    assert!(spider_crab.contains_page(&parsed_url));
 }
 
 #[tokio::test]
@@ -72,20 +66,14 @@ async fn test_two_pages() {
     mock1.assert();
     mock2.assert();
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains two pages
-    assert_eq!(graph.node_count(), 2);
+    assert_eq!(spider_crab.page_count(), 2);
 
     // Make sure that the page graph contains four links
-    assert_eq!(graph.edge_count(), 4);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 4);
 
     // Make sure that the page map contains the home page
-    assert!(map.contains_key(&parsed_url));
-
-    // Make sure there are two pages in the page map
-    assert_eq!(map.len(), 2);
+    assert!(spider_crab.contains_page(&parsed_url));
 }
 
 #[tokio::test]
@@ -116,20 +104,14 @@ async fn test_missing_page() {
     // Make sure that visit _website() returned false
     assert!(!success);
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains two pages
-    assert_eq!(graph.node_count(), 2);
+    assert_eq!(spider_crab.page_count(), 2);
 
     // Make sure there is only one link in the page graph
-    assert_eq!(graph.edge_count(), 1);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 1);
 
     // Make sure that the page map contains the mock page
-    assert!(map.contains_key(&parsed_url));
-
-    // Make sure there are two pages in the page map
-    assert_eq!(map.len(), 2);
+    assert!(spider_crab.contains_page(&parsed_url));
 }
 
 #[tokio::test]
@@ -155,20 +137,14 @@ async fn test_missing_href() {
     // Make sure that visit _website() returned false
     assert!(!success);
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains one page
-    assert_eq!(graph.node_count(), 1);
+    assert_eq!(spider_crab.page_count(), 1);
 
     // Make sure there are no links in the page graph
-    assert_eq!(graph.edge_count(), 0);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 0);
 
     // Make sure that the page map contains the mock page
-    assert!(map.contains_key(&parsed_url));
-
-    // Make sure there is only one page in the page map
-    assert_eq!(map.len(), 1);
+    assert!(spider_crab.contains_page(&parsed_url));
 }
 
 #[tokio::test]
@@ -194,20 +170,14 @@ async fn test_empty_href() {
     // Make sure that visit _website() returned false
     assert!(!success);
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains one page
-    assert_eq!(graph.node_count(), 1);
+    assert_eq!(spider_crab.page_count(), 1);
 
     // Make sure there are no links in the page graph
-    assert_eq!(graph.edge_count(), 0);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 0);
 
     // Make sure that the page map contains the mock page
-    assert!(map.contains_key(&parsed_url));
-
-    // Make sure there is only one page in the page map
-    assert_eq!(map.len(), 1);
+    assert!(spider_crab.contains_page(&parsed_url));
 }
 
 #[tokio::test]
@@ -240,19 +210,77 @@ async fn test_empty_href_in_second_page() {
     // Make sure that visit _website() returned false
     assert!(!success);
 
-    let graph = &spider_crab.graph;
     // Make sure that the page graph contains two pages
-    assert_eq!(graph.node_count(), 2);
+    assert_eq!(spider_crab.page_count(), 2);
 
     // Make sure there are is only one link in the graph
-    assert_eq!(graph.edge_count(), 1);
-
-    let map = &spider_crab.map;
+    assert_eq!(spider_crab.link_count(), 1);
 
     // Make sure that the page map contains the mock page
-    assert!(map.contains_key(&parsed_url));
-    assert!(map.contains_key(&parsed_url.join("pageB.html").unwrap()));
+    assert!(spider_crab.contains_page(&parsed_url));
+    assert!(spider_crab.contains_page(&parsed_url.join("pageB.html").unwrap()));
+}
+
+
+#[tokio::test]
+async fn test_empty_content_type() {
+    let mut server = Server::new();
+
+    let url = server.url();
+    let parsed_url = Url::parse(url.as_str()).unwrap();
+
+    let mock = server.mock("GET", "/")
+      .with_status(201)
+      .with_header("content-type", "text/html")
+      .with_body("<!DOCTYPE html><html><body><a href=\"pageB.html\">This is a link to page B.</a></body></html>")
+      .create();
+
+    let mock_page_b = server.mock("GET", "/pageB.html")
+      .with_status(201)
+      .with_header("content-type", "application/javascript")
+      .with_body("alert(\"Hello world!\");")
+      .create();
+
+    let mut spider_crab = SpiderCrab::new(&[url.as_str()]);
+
+    let success = spider_crab.visit_website(url.as_str()).await;
+
+    // Make sure the HTTP request was made to the first page
+    mock.assert();
+    mock_page_b.assert();
+
+    // Make sure that visit _website() returned false
+    assert!(!success);
+
+    // Make sure that the page graph contains two pages
+    assert_eq!(spider_crab.page_count(), 2);
+
+    // Make sure there are is only one link in the graph
+    assert_eq!(spider_crab.link_count(), 1);
+
+    // Make sure that the page map contains the mock page
+    assert!(spider_crab.contains_page(&parsed_url));
+    assert!(spider_crab.contains_page(&parsed_url.join("pageB.html").unwrap()));
 
     // Make sure there are two pages in the page map
-    assert_eq!(map.len(), 2);
+    assert_eq!(spider_crab.map.len(), 2);
+
+    // Check the root page
+    {
+      // Make sure that the root page's content type is correct
+      let page_a_weight: &crate::Page = spider_crab.get_page(&parsed_url);
+      assert_eq!(page_a_weight.content_type.as_ref().unwrap(), "text/html");
+      assert!(page_a_weight.checked);
+      assert_eq!(page_a_weight.status_code.unwrap(), 201);
+      assert_eq!(page_a_weight.errors.len(), 0);
+    }
+
+    {
+      // Make sure that page B's content type is correct
+      let page_b_weight = spider_crab.get_page(&parsed_url.join("/pageB.html").unwrap());
+      assert_eq!(page_b_weight.content_type.as_ref().unwrap(), "application/javascript");
+      assert!(page_b_weight.checked);
+      assert_eq!(page_b_weight.status_code.unwrap(), 201);
+      assert_eq!(page_b_weight.errors.len(), 0);
+    }
 }
