01a7c0393d2c836b8fa8039b456366527f5914bf
Tyler Sengia <tylersengia@gmail.com>
2023-12-17 00:28:28 -0500

    Remove duplicated unit test

--- src/positive_tests.rs
+++ src/positive_tests.rs
@@ -132,69 +132,6 @@ async fn test_helper_functions() {
 }
 
 #[tokio::test]
-async fn test_empty_content_type() {
-    let mut server = Server::new();
-
-    let url = server.url();
-    let parsed_url = Url::parse(url.as_str()).unwrap();
-
-    let mock = server.mock("GET", "/")
-      .with_status(201)
-      .with_header("content-type", "text/html")
-      .with_body("<!DOCTYPE html><html><body><a href=\"script.html\">This is a link to some javascript.</a></body></html>")
-      .create();
-
-    let mock_page_b = server
-        .mock("GET", "/script.html")
-        .with_status(201)
-        .with_body("alert(\"Hello world!\");")
-        .create();
-
-    let mut spider_crab = SpiderCrab::new(&[url.as_str()]);
-
-    let success = spider_crab.visit_website(url.as_str()).await;
-
-    // Make sure the HTTP request was made to the first page
-    mock.assert();
-    mock_page_b.assert();
-
-    // Make sure that visit _website() returned true
-    assert!(success);
-
-    // Make sure that the page graph contains two pages
-    assert_eq!(spider_crab.page_count(), 2);
-
-    // Make sure there are is only one link in the graph
-    assert_eq!(spider_crab.link_count(), 1);
-
-    // Make sure that the page map contains the mock page
-    assert!(spider_crab.contains_page(&parsed_url));
-    assert!(spider_crab.contains_page(&parsed_url.join("script.html").unwrap()));
-
-    // Make sure there are two pages in the page map
-    assert_eq!(spider_crab.map.len(), 2);
-
-    // Check the root page
-    {
-        // Make sure that the root page's content type is correct
-        let page_a_weight: &crate::Page = spider_crab.get_page(&parsed_url);
-        assert_eq!(page_a_weight.content_type.as_ref().unwrap(), "text/html");
-        assert!(page_a_weight.checked);
-        assert_eq!(page_a_weight.status_code.unwrap(), 201);
-        assert_eq!(page_a_weight.errors.len(), 0);
-    }
-
-    {
-        // Make sure that page B's content type is correct
-        let page_b_weight = spider_crab.get_page(&parsed_url.join("/pageB.html").unwrap());
-        assert!(page_b_weight.content_type.is_none());
-        assert!(page_b_weight.checked);
-        assert_eq!(page_b_weight.status_code.unwrap(), 201);
-        assert_eq!(page_b_weight.errors.len(), 0);
-    }
-}
-
-#[tokio::test]
 async fn test_skip_link_class() {
     let mut server = Server::new();
 
